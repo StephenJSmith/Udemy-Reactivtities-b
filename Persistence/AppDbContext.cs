4487c91fa70138965d888efe43b978a8610983ef
Stephen Smith <stephensmith_aus@yahoo.com.au>
2025-02-22 17:08:12 +1100

    End of Section 2

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+  public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+  {
+    public required DbSet<Activity> Activities { get; set; }
+  }
+}
