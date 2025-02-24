35dbf08339aa4f0da765ec8cffc13493d7ffba66
Stephen Smith <stephensmith_aus@yahoo.com.au>
2025-02-25 05:49:04 +1100

    End of section 5

---
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,59 @@
+import { Grid2 } from "@mui/material";
+import { Activity } from "../../../lib/types";
+import ActivityList from "./ActivityList";
+import ActivityDetail from "../details/ActivityDetail";
+import ActivityForm from "../form/ActivityForm";
+
+type Props = {
+  activities: Activity[];
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
+  selectedActivity?: Activity;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  editMode: boolean;
+  submitForm: (activity: Activity) => void;
+  deleteActivity: (id: string) => void;
+};
+
+const ActivityDashboard = ({
+  activities,
+  selectActivity,
+  cancelSelectActivity,
+  selectedActivity,
+  openForm,
+  closeForm,
+  editMode,
+  submitForm,
+  deleteActivity,
+}: Props) => {
+  return (
+    <Grid2 container spacing={3}>
+      <Grid2 size={7}>
+        <ActivityList 
+          activities={activities} 
+          selectActivity={selectActivity} 
+          deleteActivity={deleteActivity}
+        />
+      </Grid2>
+      <Grid2 size={5}>
+        {selectedActivity && !editMode && (
+          <ActivityDetail
+            activity={selectedActivity}
+            cancelSelectActivity={cancelSelectActivity}
+            openForm={openForm}
+          />
+        )}
+        {editMode && 
+          <ActivityForm 
+            closeForm={closeForm} 
+            activity={selectedActivity} 
+            submitForm={submitForm}
+          />
+        }
+      </Grid2>
+    </Grid2>
+  );
+};
+
+export default ActivityDashboard;
---
+++ client/src/features/activities/details/ActivityDetail.tsx
@@ -0,0 +1,39 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { Activity } from "../../../lib/types";
+
+type Props = {
+  activity: Activity;
+  cancelSelectActivity: () => void
+  openForm: (id: string) => void
+};
+
+const ActivityDetail = ({ activity, cancelSelectActivity, openForm: openform }: Props) => {
+  return (
+    <Card sx={{ borderRadius: 3 }}>
+      <CardMedia
+        component="img"
+        src={`/images/categoryImages/${activity.category}.jpg`}
+      />
+      <CardContent>
+        <Typography variant="h5">{activity.title}</Typography>
+        <Typography variant="subtitle1" fontWeight="light">
+          {activity.date}
+        </Typography>
+        <Typography variant="body1">{activity.description}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button onClick={() => openform(activity.id)} color="primary">Edit</Button>
+        <Button onClick={cancelSelectActivity} color="inherit">Cancel</Button>
+      </CardActions>{" "}
+    </Card>
+  );
+};
+
+export default ActivityDetail;
