f995420b90d302c60dd77bbe3011f0f3e7430dbf
Stephen Smith <stephensmith_aus@yahoo.com.au>
2025-02-28 05:53:38 +1100

    End of section 9

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -3,19 +3,25 @@ import {
   AppBar,
   Box,
   Container,
+  LinearProgress,
   MenuItem,
   Toolbar,
   Typography,
 } from "@mui/material";
 import { NavLink } from "react-router";
 import MenuItemLink from "../shared/components/MenuItemLink";
+import { useStore } from "../../lib/hooks/useStore";
+import { Observer } from "mobx-react-lite";
 
 const NavBar = () => {
+  const {uiStore} = useStore();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
         position="static"
         sx={{
+          position: 'relative',
           backgroundImage:
             "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)",
         }}
@@ -41,12 +47,32 @@ const NavBar = () => {
               <MenuItemLink to='/createActivity'>
                 Create Activity
               </MenuItemLink>
+              <MenuItemLink to='/counter'>
+                Counter
+              </MenuItemLink>
             </Box>
             <MenuItem>
               User menu
             </MenuItem>
           </Toolbar>
         </Container>
+
+        <Observer>
+          {() => uiStore.isLoading 
+          ? (
+            <LinearProgress 
+              color='secondary'
+              sx={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                height: 4
+              }}
+            />
+          )
+        : null}
+        </Observer>
       </AppBar>
     </Box>
   );
--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => setTimeout(resolve, delay));
@@ -6,6 +7,12 @@ const sleep = (delay: number) => {
 
 const agent = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+});
+
+agent.interceptors.request.use(config => {
+  store.uiStore.isBusy();
+
+  return config;
 });
 
 agent.interceptors.response.use(async (response) => {
@@ -17,6 +24,8 @@ agent.interceptors.response.use(async (response) => {
     console.log(error);
 
     return Promise.reject(error);
+  } finally {
+    store.uiStore.isIdle();
   }
 });
 
