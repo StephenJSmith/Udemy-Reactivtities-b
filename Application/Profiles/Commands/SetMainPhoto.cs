3849a660e3ddb7fa7849d7502ebbd6b72c4b82a8
Stephen Smith <stephensmith_aus@yahoo.com.au>
2025-03-13 05:41:40 +1100

    End of section 16

---
+++ Application/Profiles/Commands/SetMainPhoto.cs
@@ -0,0 +1,34 @@
+using System;
+using Application.Core;
+using Application.Interfaces;
+using MediatR;
+using Persistence;
+
+namespace Application.Profiles.Commands;
+
+public class SetMainPhoto
+{
+  public class Command : IRequest<Result<Unit>>
+  {
+    public required string PhotoId { get; set; }
+  }
+
+  public class Handler(
+    AppDbContext context,
+    IUserAccessor userAccessor) : IRequestHandler<Command, Result<Unit>>
+  {
+    public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+    {
+      var user = await userAccessor.GetUserWithPhotosAsync();
+      var photo = user.Photos.FirstOrDefault(x => x.Id == request.PhotoId);
+      if (photo == null) return Result<Unit>.Failure("Photo not found", 400);
+
+      user.ImageUrl = photo.Url;
+
+      var result = await context.SaveChangesAsync(cancellationToken) > 0;
+      if (!result) return Result<Unit>.Failure("Failed to save photo in DB", 400);
+
+      return Result<Unit>.Success(Unit.Value);
+    }
+  }
+}
